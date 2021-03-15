19e1b91dcc24a8d60e75b2f170c606e04b866e23
gregorie vexier <gregorievexier@MacBook-Pro-de-gregorie.local>
2021-03-15 16:52:09 +0100

    template

--- resources/views/monstres.blade.php
+++ resources/views/monstres.blade.php
@@ -1,28 +1,19 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    hello
-    @foreach($monstres as $monstre)
-        <div>
-            <h1>{{$monstre->name}}</h1>
-            <p>{{$monstre->description}}</p>
-        </div>
-    @endforeach
+@extends('template')
 
-    @if(Session::has('error'))
-    <div class="alert alert-danger">
-        {{ Session::get('error') }}
+@section('content')
+hello
+@foreach($monstres as $monstre)
+    <div>
+        <h1>{{$monstre->name}}</h1>
+        <p>{{$monstre->description}}</p>
     </div>
-    @endif
+@endforeach
 
-    <h1>{{ $id }}</h1>
+@if(Session::has('error'))
+<div class="alert alert-danger">
+    {{ Session::get('error') }}
+</div>
+@endif
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+<h1>{{ $id }}</h1>
+@endsection
